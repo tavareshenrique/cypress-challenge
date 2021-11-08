cdec1d426bd5ddc162f9833581fb0d0bb35641b5
Henrique Tavares <ihenrits@gmail.com>
2021-11-08 19:44:04 -0300

    add locators

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -2,44 +2,45 @@
 
 import faker from 'faker';
 
+import locators from '../support/locators';
+
 describe('Should test at a functional level', () => {
   before(() => {
     cy.visit('http://barrigareact.wcaquino.me/');
 
-    cy.get('[data-test=email]').type('ihenrits@gmail.com')
-    cy.get('[data-test=passwd]').type('123123')
-    cy.get('.btn').click()
+    cy.get(locators.LOGIN.USER).type('ihenrits@gmail.com')
+    cy.get(locators.LOGIN.PASSWORD).type('123123')
+    cy.get(locators.LOGIN.BTN_LOGIN).click()
 
-    cy.get('.toast-message').should('contain', 'Bem vindo')
+    cy.get(locators.MESSAGE).should('contain', 'Bem vindo')
   })
 
   it('should create an account', () => {
     const accountName = faker.finance.accountName();
 
-    cy.get('[data-test=menu-settings]').click()
-    cy.get('[href="/contas"]').click()
+    cy.get(locators.MENU.SETTINGS).click()
+    cy.get(locators.MENU.ACCOUNTS).click()
 
-    cy.get('[data-test=nome]').type(accountName)
-    cy.get('.btn').click()
+    cy.get(locators.ACCOUNTS.NOME).type(accountName)
+    cy.get(locators.ACCOUNTS.BTN_SAVE).click()
 
-    cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
-    cy.get('.container').should('contain', accountName)
+    cy.get(locators.MESSAGE).should('contain', 'Conta inserida com sucesso')
   })
 
   it.only('should update an account', () => {
     const accountName = faker.finance.accountName();
 
-    cy.get('[data-test=menu-settings]').click()
-    cy.get('[href="/contas"]').click()
+    cy.get(locators.MENU.SETTINGS).click()
+    cy.get(locators.MENU.ACCOUNTS).click()
 
-    cy.xpath(" //table//td[contains(., 'Investment Account')]/..//i[@class='far fa-edit']").click()
-    cy.get('[data-test=nome]')
+    cy.xpath(locators.ACCOUNTS.XP_BTN_CHANGE).click()
+
+    cy.get(locators.ACCOUNTS.NOME)
       .clear()
       .type(accountName)
-    cy.get('.btn').click()
+    cy.get(locators.ACCOUNTS.BTN_SAVE).click()
 
-    cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso')
-    cy.get('.container').should('contain', accountName)
+    cy.get(locators.MESSAGE).should('contain', 'Conta atualizada com sucesso')
   })
 })
 
---
+++ cypress/support/locators.js
@@ -0,0 +1,19 @@
+const locators = {
+  LOGIN: {
+    USER: '[data-test=email]',
+    PASSWORD: '[data-test=passwd]',
+    BTN_LOGIN: '.btn'
+  },
+  MENU: {
+    SETTINGS: '[data-test=menu-settings]',
+    ACCOUNTS: '[href="/contas"]',
+  },
+  ACCOUNTS: {
+    NOME: '[data-test=nome]',
+    BTN_SAVE: '.btn',
+    XP_BTN_CHANGE: "//table//td[contains(., 'Investment Account')]/..//i[@class='far fa-edit']"
+  },
+  MESSAGE: '.toast-message',
+}
+
+export default locators;
