a8f1e9b1c4e0d1f4c9a152576d67602f8c90b784
Henrique Tavares <ihenrits@gmail.com>
2021-11-08 19:55:14 -0300

    account commands

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -2,6 +2,8 @@
 
 import faker from 'faker';
 
+import '../support/commandsAccount'
+
 import locators from '../support/locators';
 
 describe('Should test at a functional level', () => {
@@ -10,19 +12,16 @@ describe('Should test at a functional level', () => {
     cy.resetApp()
   })
 
-  it('should create an account', () => {
+  it.only('should create an account', () => {
     const accountName = faker.finance.accountName();
 
-    cy.get(locators.MENU.SETTINGS).click()
-    cy.get(locators.MENU.ACCOUNTS).click()
-
-    cy.get(locators.ACCOUNTS.NOME).type(accountName)
-    cy.get(locators.ACCOUNTS.BTN_SAVE).click()
+    cy.accessAccountMenu()
+    cy.addAccount(accountName)
 
     cy.get(locators.MESSAGE).should('contain', 'Conta inserida com sucesso')
   })
 
-  it.only('should update an account', () => {
+  it('should update an account', () => {
     const accountName = faker.finance.accountName();
 
     cy.get(locators.MENU.SETTINGS).click()
---
+++ cypress/support/commandsAccount.js
@@ -0,0 +1,11 @@
+import locators from './locators'
+
+Cypress.Commands.add('accessAccountMenu', () => {
+  cy.get(locators.MENU.SETTINGS).click()
+  cy.get(locators.MENU.ACCOUNTS).click()
+})
+
+Cypress.Commands.add('addAccount', (accountName) => {
+  cy.get(locators.ACCOUNTS.NOME).type(accountName)
+  cy.get(locators.ACCOUNTS.BTN_SAVE).click()
+})
