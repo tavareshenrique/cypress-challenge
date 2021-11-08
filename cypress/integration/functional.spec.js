bce959dd04bab2aef0c8dae38459f29df3243798
Henrique Tavares <ihenrits@gmail.com>
2021-11-08 19:16:10 -0300

    add account e2e

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+import faker from 'faker';
+
 describe('Should test at a functional level', () => {
   before(() => {
     cy.visit('http://barrigareact.wcaquino.me/');
@@ -11,8 +13,17 @@ describe('Should test at a functional level', () => {
     cy.get('.toast-message').should('contain', 'Bem vindo')
   })
 
-  it('teste', () => {
-    
+  it('should create an account', () => {
+    const accountName = faker.finance.accountName();
+
+    cy.get('[data-test=menu-settings]').click()
+    cy.get('[href="/contas"]').click()
+
+    cy.get('[data-test=nome]').type(accountName)
+    cy.get('.btn').click()
+
+    cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    cy.get('.container').should('contain', accountName)
   })
 })
 
