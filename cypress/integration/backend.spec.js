81a1ea7b15aa3d4aaff68b133c4f589d03384028
Henrique Tavares <ihenrits@gmail.com>
2021-11-10 19:42:36 -0300

    inser movimentation test and add eslint and prettier

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -6,90 +6,99 @@ describe('Should test at a functional level', () => {
   before(() => {
     cy.getToken('ihenrits@gmail.com', '123123')
       .then((tokenResponse) => {
-        token = tokenResponse
-      })
-  })
+        token = tokenResponse;
+      });
+  });
 
   beforeEach(() => {
-    cy.resetRest(token)
-  })
+    cy.resetRest(token);
+  });
 
   it('should create an account', () => {
     cy.request({
       method: 'POST',
       url: '/contas',
       headers: {
-        Authorization: `JWT ${token}`
+        Authorization: `JWT ${token}`,
       },
       body: {
         nome: 'Conta via rest',
       },
-    }).as('response') 
+    }).as('response');
 
-    cy.get('@response').then(res => {
-      expect(res.status).to.be.equal(201)
-      expect(res.body).to.have.property('id')
-      expect(res.body).to.have.property('nome', 'Conta via rest')
-    })   
-  })
+    cy.get('@response').then((res) => {
+      expect(res.status).to.be.equal(201);
+      expect(res.body).to.have.property('id');
+      expect(res.body).to.have.property('nome', 'Conta via rest');
+    });
+  });
 
   it('should update an account', () => {
-    cy.request({
-      method: 'GET',
-      url: '/contas',
-      headers: {
-        Authorization: `JWT ${token}`
-      },
-      qs: {
-        nome: 'Conta para alterar',
-      },
-    }).then(res => {
+    cy.getAccountByName('Conta para movimentacoes', token).then((accountId) => {
       cy.request({
         method: 'PUT',
-        url: `/contas/${res.body[0].id}`,
+        url: `/contas/${accountId}`,
         headers: {
-          Authorization: `JWT ${token}`
+          Authorization: `JWT ${token}`,
         },
         body: {
           nome: 'Conta elterada via rest',
-        }
-      }).as('response')
+        },
+      }).as('response');
 
-      cy.get('@response').its('status').should('be.equal', 200)
-    })
+      cy.get('@response').its('status').should('be.equal', 200);
+    });
+  });
 
-    
-  })
-
-  it.only('should not create an account with same name', () => {
+  it('should not create an account with same name', () => {
     cy.request({
       method: 'POST',
       url: '/contas',
       headers: {
-        Authorization: `JWT ${token}`
+        Authorization: `JWT ${token}`,
       },
       body: {
         nome: 'Conta mesmo nome',
       },
       failOnStatusCode: false,
-    }).as('response') 
+    }).as('response');
 
-    cy.get('@response').then(res => {
-      expect(res.status).to.be.equal(400)
-      expect(res.body).to.have.property('error', 'Já existe uma conta com esse nome!')
-    })   
-  })
+    cy.get('@response').then((res) => {
+      expect(res.status).to.be.equal(400);
+      expect(res.body).to.have.property('error', 'Já existe uma conta com esse nome!');
+    });
+  });
 
-  it('should create a transaction', () => {
-    
-  })
+  it.only('should create a transaction', () => {
+    cy.getAccountByName('Conta para movimentacoes', token).then((accountId) => {
+      cy.request({
+        method: 'POST',
+        url: '/transacoes',
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+        body: {
+          conta_id: accountId,
+          data_pagamento: Cypress.moment().add({ days: 1 }).format('DD/MM/YYYY'),
+          data_transacao: Cypress.moment().format('DD/MM/YYYY'),
+          descricao: 'Transação via rest',
+          envolvido: 'Itaú',
+          status: true,
+          tipo: 'REC',
+          valor: '123',
+        },
+      }).as('response');
+    });
+
+    cy.get('@response').its('status').should('be.equal', 201);
+    cy.get('@response').its('body.id').should('exist');
+  });
 
   it('should get balance', () => {
-    
-  })
+
+  });
 
   it('should remove a transaction', () => {
-   
-  })
-})
 
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,17 @@
-import locators from './locators'
+import locators from './locators';
 
 Cypress.Commands.add('login', (user, password) => {
   cy.visit('http://barrigareact.wcaquino.me/');
-  cy.get(locators.LOGIN.USER).type(user)
-  cy.get(locators.LOGIN.PASSWORD).type(password)
-  cy.get(locators.LOGIN.BTN_LOGIN).click()
-  cy.get(locators.MESSAGE).should('contain', 'Bem vindo', { timeout: 10000 })
-})
+  cy.get(locators.LOGIN.USER).type(user);
+  cy.get(locators.LOGIN.PASSWORD).type(password);
+  cy.get(locators.LOGIN.BTN_LOGIN).click();
+  cy.get(locators.MESSAGE).should('contain', 'Bem vindo', { timeout: 10000 });
+});
 
 Cypress.Commands.add('resetApp', () => {
-  cy.get(locators.MENU.SETTINGS).click()
-  cy.get(locators.MENU.RESET).click()
-})
+  cy.get(locators.MENU.SETTINGS).click();
+  cy.get(locators.MENU.RESET).click();
+});
 
 Cypress.Commands.add('getToken', (user, password) => {
   cy.request({
@@ -20,20 +20,31 @@ Cypress.Commands.add('getToken', (user, password) => {
     body: {
       email: user,
       senha: password,
-      redirecionar: false
-    }
+      redirecionar: false,
+    },
   }).its('body.token').should('not.be.empty')
-      .then(token => {
-        return token
-      })
-})
+    .then((token) => token);
+});
 
 Cypress.Commands.add('resetRest', (token) => {
   cy.request({
     method: 'GET',
     headers: {
-      Authorization: `JWT ${token}`
+      Authorization: `JWT ${token}`,
     },
     url: '/reset',
-  }).its('status').should('be.equal', 200)
-})
+  }).its('status').should('be.equal', 200);
+});
+
+Cypress.Commands.add('getAccountByName', (accountName, token) => {
+  cy.request({
+    method: 'GET',
+    url: '/contas',
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+    qs: {
+      nome: accountName,
+    },
+  }).then((res) => res.body[0].id);
+});
